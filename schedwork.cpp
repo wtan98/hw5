71288f4334ce5667c8c361f522c5a2009e4c6490
Wei Ye Tan Liu <wtanliu@usc.edu>
2025-12-06 04:48:57 +0000

    HW submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,12 +6,8 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
-
 #endif
-
 #include "schedwork.h"
-
 using namespace std;
 
 // a constant that can be used to indicate an INVALID 
@@ -19,9 +15,8 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
+bool helper(size_t day, DailySchedule& schedule, size_t dailyWorkerCount, size_t currWorker, size_t maxShiftCount, vector<size_t>& workerShiftCounts, const AvailabilityMatrix& available);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +33,68 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t dayCount = avail.size(); 
+    size_t workerCount = avail[0].size(); 
 
+    //set the schedule to number of days 
+    sched.resize(dayCount);
 
+    vector<size_t> workerShifts(workerCount);    //make a list to track shifts worked per worker
+    for (size_t i =0; i < workerCount; i++){
+        workerShifts[i] = 0; //start with 0 shifts 
+    }
 
+    return helper(0, sched, dailyNeed, 0, maxShifts, workerShifts, avail);
 }
 
+bool helper(size_t day, DailySchedule& schedule, size_t dailyWorkerCount, size_t currWorker, size_t maxShiftCount, vector<size_t>& workerShiftCounts, const AvailabilityMatrix& available){
+    size_t dayCount = available.size(); 
+    size_t workerCount = available[0].size(); 
+
+    //check if the days are filled 
+    if (day == dayCount){
+        return true;    //done
+    }
+
+    //check if day has enough workers 
+    size_t scheduledWorkerCount = schedule[day].size(); 
+    if (scheduledWorkerCount == dailyWorkerCount){
+        //go to next day 
+        return helper(day+1, schedule, dailyWorkerCount, 0, maxShiftCount, workerShiftCounts, available); 
+    }
+
+    //there aren't enough workers scheduled and all workers have been tried 
+    if(currWorker >= workerCount){
+        return false;   //backtrack
+    }
+    
+    //check if worker is already scheduled for this day
+    bool working = false;
+    for (size_t i = 0; i < schedule[day].size(); i++){
+        if (schedule[day][i] == currWorker){
+            working = true; 
+            break;
+        }
+    }
+
+    bool availableToWork = available[day][currWorker]; //worker must be available to work
+    bool maxShiftSafe = workerShiftCounts[currWorker] < maxShiftCount; //worker must not exceed maxShiftCount
+    //add needed worker to the day
+    if (availableToWork == true && maxShiftSafe == true && working == false){
+        
+        schedule[day].push_back(currWorker);    
+        workerShiftCounts[currWorker]++;    //update shift count
+
+        //try adding the next worker  
+        bool canAdd = helper(day, schedule, dailyWorkerCount, currWorker + 1, maxShiftCount, workerShiftCounts, available); 
+        if (canAdd == true){
+            return true; 
+        }
+        //else there is no one to add; backtrack and update count 
+        else{
+            schedule[day].pop_back(); 
+            workerShiftCounts[currWorker] -= 1; 
+        }
+    }
+    return helper(day, schedule, dailyWorkerCount, currWorker + 1, maxShiftCount, workerShiftCounts, available); 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void helper(int curr, string pattern, string possibleWords, string& currWord, set<string>& correctWords, const set<string>& dict); 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,64 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> correctWords; 
 
+    string currWord = in;
+
+    helper(0, in, floating, currWord, correctWords, dict);
+
+    return correctWords; 
 }
 
 // Define any helper functions here
+void helper(int curr, string pattern, string possibleLetters, string& currWord, set<string>& correctWords, const set<string>& dict)
+{ 
+    //check if all the spots have been filled
+    if (curr == int(pattern.size())){
+      if (possibleLetters.size() == 0){ //all possible letters used 
+        //check if word is real 
+        if (dict.find(currWord) != dict.end()){
+          correctWords.insert(currWord);
+        }
+      }
+      return; //done
+    }
+
+    //go next if there is already a letter 
+    if(pattern[curr] != '-'){
+      helper(curr+1, pattern, possibleLetters, currWord, correctWords, dict);
+      return; 
+    }
+
+    //get the number of spots that are blank; is a "-"
+    int blankCount = 0; 
+    for (int i = curr; i < int(pattern.size()); i++){
+      if (pattern[i] == '-'){
+        blankCount++; 
+      }
+    }
+
+    //first try the letters from possibleLetters
+    for (int i = 0; i < int(possibleLetters.size()); i++){
+      
+      //add possible letters to the spot 
+      char c = possibleLetters[i];
+      currWord[curr] = c; 
+
+      string newPossibleLetters = possibleLetters;  //make a new string without that letter 
+      newPossibleLetters.erase(i, 1); 
+
+      helper(curr+1, pattern, newPossibleLetters, currWord, correctWords, dict);
+    }
+
+    //otherwise, try regular letters iif there is space; blanks > possibleLetters
+    if (blankCount > int(possibleLetters.size())){
+      for(char a = 'a'; a <= 'z'; a++){
+        currWord[curr] = a; 
+        
+        //go next 
+        helper(curr+1, pattern, possibleLetters, currWord, correctWords, dict);
+      }
+    }
+    
+}
